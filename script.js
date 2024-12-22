68d8c1d686aa1ad23d3e8b005be21c1e91c927d2
Aryan Rai <aryanrai0110@gmail.com>
2024-12-22 17:27:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,59 +3,55 @@ let intervalId;
 
 onLoad();
 
-function onLoad(){
+function onLoad() {
   displayImages(id);
   startInterval();
 }
 
-function startInterval(){
+function startInterval() {
   clearInterval(intervalId);
   intervalId = setInterval(() => {
     onRightClick();
   }, 3000);
 }
 
-function displayImages(id){
-  let frame = document.querySelector('.container');
+function displayImages(id) {
+  let frame = document.querySelector(".container");
   frame.innerHTML = `
-    <button class="left" onclick="onLeftClick()"> < </button>
-    <div class="images">
-      <img src="${getElement(id)}" alt="item images${id}" class="image"/>
-    </div>
-    <button class="right" onclick="onRightClick()"> > </button>
+    <button class="left w-[10%] lg:w-[4%] h-[100%] flex items-center justify-center bg-white text-2xl absolute left-0 opacity-0 hover:opacity-[0.5]" onclick="onLeftClick()"><i class="fa-solid fa-chevron-left"></i></button>
+    <div class="images w-[100%] h-[100%] bg-[url('${getElement(
+      id
+    )}')] bg-cover"></div>
+    <button class="right w-[10%] lg:w-[4%] h-[100%] flex items-center justify-center bg-white text-2xl absolute right-0 opacity-0 hover:opacity-[0.5]" onclick="onRightClick()"><i class="fa-solid fa-chevron-right"></i></button>
   `;
 }
 
-function getElement(idx){
-  for(let i = 0; i < imageList.length; i++){
-    if(idx == imageList[i].id){
+function getElement(idx) {
+  for (let i = 0; i < imageList.length; i++) {
+    if (idx == imageList[i].id) {
       return imageList[i].source;
     }
   }
-};
+}
 
-let left_btn = document.querySelector('.left');
+let leftB = document.querySelector(".left");
+let rightB = document.querySelector(".right");
 
-let right_btn = document.querySelector('.right');
-
-function onLeftClick(){
-  if(id == 1){
+function onLeftClick() {
+  if (id == 1) {
     id = imageList.length;
-  }
-  else{
+  } else {
     id--;
   }
   displayImages(id);
   startInterval();
-};
-
-function onRightClick(){
-  if(id == imageList.length){
+}
+function onRightClick() {
+  if (id == imageList.length) {
     id = 1;
-  }
-  else{
+  } else {
     id++;
   }
   displayImages(id);
   startInterval();
-};
+}
